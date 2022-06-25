2625bfb555bd78b846e70e7f90f3464e5acf1dbe
Raditya <raditya.learner@gmail.com>
2022-06-25 10:16:12 +0800

    Initial Commit

---
+++ ToDos/ToDosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDosApp.swift
+//  ToDos
+//
+//  Created by CTSS Students on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
